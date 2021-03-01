604ed5e59348ae181ebceb11113dedce08fd03db
AR-Shahin <mdshahinmije96@gmail.com>
2021-03-01 20:27:53 +0600

    first commit

---
+++ class/index.php
@@ -0,0 +1,17 @@
+<?php 
+require_once 'allclasses.php';
+
+
+$a = new Math();
+echo $a->sum(10,20);
+echo '<br>';
+$x = new any();
+$y = new any();
+$x->name('shahin');
+$x->name('omi');
+$y->name('sss');
+
+$aa = new store();
+$aa->set('ars','anisur rahman');
+
+
