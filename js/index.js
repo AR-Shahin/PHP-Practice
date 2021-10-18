7deaeafbce15019ff5580705d0f84d48140053b1
AR Shahin <mdshahinmije96@gmail.com>
2021-10-18 23:02:10 +0600

    img path

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var img = document.createElement("img");
-  img.setAttribute("src", `${window.location.origin}/mash.png`);
+  img.setAttribute("src", `https://github.com/AR-Shahin/OOP_practice_with_PHP/blob/master/mash.png`);
 //   img.style.width = `50%`;
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
